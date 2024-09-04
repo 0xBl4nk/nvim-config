5d344643ad598c71fd634af029384321a9935575
ArthurHydr <61481946+ArthurHydr@users.noreply.github.com>
2024-09-04 12:26:23 -0300

    [*] FIX .html crash

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -6,5 +6,21 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
   highlight = {
     enable = true,
-  },
+    disable = function(lang, buf)
+        if lang == "html" then
+             return true
+        end
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            vim.notify(
+                "File larger than 100KB treesitter disabled for performance",
+                vim.log.levels.WARN,
+                {title = "Treesitter"}
+            )
+            return true
+        end
+    end,
+    additional_vim_regex_highlighting = false,
+  } 
 }
